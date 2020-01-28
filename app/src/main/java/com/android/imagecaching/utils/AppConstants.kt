310f36aadee0bea90554caae056238295faeab6a
Bala <pbalasubramaniammca@gmail.com>
2020-01-28 23:15:42 +0700

    Update AppConstants.kt

--- app/src/main/java/com/android/imagecaching/utils/AppConstants.kt
+++ app/src/main/java/com/android/imagecaching/utils/AppConstants.kt
@@ -1,6 +1,6 @@
 package com.android.imagecaching.utils
 
 
-const val IS_TEST_BUILD = true
+const val IS_TEST_BUILD = false
 const val BASE_URL      = "https://pastebin.com/"
 const val DATA_API      = "raw/wgkJgazE/"
